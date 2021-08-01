63f068353ecfc04bff9617fcc00ad32f7fb0ff39
Rossanne Clamp <ross@Rossannes-MacBook-Pro.local>
2021-08-01 13:24:16 -0700

    axios messages structure added

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,13 +1,28 @@
+import axios from "axios";
 import React from "react";
 
-function Messages() {
+const baseURL = "http://localhost:3002/messages";
+// const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+
+export default function Messages() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+      console.log(response)
+    });
+  }, []);
+
+  if (!post) return null;
+
   return (
-    <div className="contact">
-    <div class="container">
-      <h1 class="font-weight-light">Messages</h1>
+    <div className="Messages">
+      <div class="container">
+        <h1 class="font-weight-light">Messages</h1>
+        <h3>{post.messages}</h3>
+        <p>{post.mood_icon_url}</p>
+      </div>
     </div>
-  </div>
   );
 }
-
-export default Messages;
