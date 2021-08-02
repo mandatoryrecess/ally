500513f9a20deef7300332f92c17b25de7f6621a
rossanne <rossanneclamp@gmail.com>
2021-08-02 14:13:40 -0700

    message styliing

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -13,42 +13,26 @@ export default function Messages(props) {
     });
   }, []);
 
-<<<<<<< HEAD
-
-=======
->>>>>>> feature/write-a-message
   if (!post) return null;
 
   const notes = post.map((messageObj) => {
-    return messageObj.messages;
-    
+    return (<div>
+            <img width="30px" src={messageObj.mood_icon_url} />
+            <span> {messageObj.messages }</span>
+            </div>
+      );
   })
 
-  const images = post.map((messageObj) => {
-    return messageObj.mood_icon_url;
-  })
 
   return (
     <div className="Messages">
       <div class="container">
-        <h1 class="font-weight-light">Messages</h1>
-<<<<<<< HEAD
-
-      <h2>Notes</h2>  {notes}
-      <h2>images</h2>  {images}
-
-=======
-        <div class="archive">
-          {post.map((messageObj) => {
-            let returnArray = [];
-            const notes = messageObj.messages;
-            const image = messageObj.mood_icon_url;
-            returnArray.push(notes, image);
-            return returnArray;
-          })}
-        </div>
->>>>>>> feature/write-a-message
+      <h2>Notes</h2>{notes}
       </div>
+      
     </div>
   );
+
+  
+
 }
