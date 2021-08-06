0fc8eaa96a26f478458cbdd3cb82e98e2265d557
cyn <cynthiacui98@gmail.com>
2021-08-06 13:57:18 -0700

    css changes

--- src/components/Ripple.jsx
+++ src/components/Ripple.jsx
@@ -4,30 +4,33 @@ import Navigation from "./Navigation";
 
 function Ripple() {
   return (
-    <div class="sunset">
+    <div className="sunani">
       <Navigation />
-      <div class="landscape">
-        <div class="sun-container">
-          <div class="sun"></div>
-        </div>
-        <div class="cloud"></div>
-        <div class="cloud cloud-1"></div>
-        <div class="sun-container sun-container-reflection">
-          <div class="sun"></div>
-        </div>
-        <div class="light"></div>
-        <div class="light light-1"></div>
-        <div class="light light-2"></div>
-        <div class="light light-3"></div>
-        <div class="light light-4"></div>
-        <div class="light light-5"></div>
-        <div class="light light-6"></div>
-        <div class="light light-7"></div>
 
-        <div class="water"></div>
+      <div class="sunset">
+        <div class="landscape">
+          <div class="sun-container">
+            <div class="sun"></div>
+          </div>
+          <div class="cloud"></div>
+          <div class="cloud cloud-1"></div>
+          <div class="sun-container sun-container-reflection">
+            <div class="sun"></div>
+          </div>
+          <div class="light"></div>
+          <div class="light light-1"></div>
+          <div class="light light-2"></div>
+          <div class="light light-3"></div>
+          <div class="light light-4"></div>
+          <div class="light light-5"></div>
+          <div class="light light-6"></div>
+          <div class="light light-7"></div>
 
-        <div class="front">
-          <div class="stone"></div>
+          <div class="water"></div>
+
+          <div class="front">
+            <div class="stone"></div>
+          </div>
         </div>
       </div>
     </div>
