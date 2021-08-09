461748945ae96caffcb4c6544c0ab4fbecf51e76
Megan V <elbowgrrl@gmail.com>
2021-08-09 11:32:47 -0700

    refactored CSS

--- src/Nightsky/Galaxy.jsx
+++ src/Nightsky/Galaxy.jsx
@@ -1,33 +1,19 @@
-import React from 'react'
+import React from "react";
 // import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 // import Rainbow from './Rainbow';
 import "./Galaxy.css";
 
+function Galaxy() {
+  const milkySky = "https://i.imgur.com/tNufwUw.png";
+  const darkSky = "https://i.imgur.com/H1bI9fo.png";
+  const cartoonStarCluster = "https://i.imgur.com/0xoT3Ky.png";
+  const rainbowStar = "https://i.imgur.com/qeOmPc0.png";
 
-  
-  function Galaxy() {
-    const milkySky = "https://i.imgur.com/tNufwUw.png";
-    const darkSky = "https://i.imgur.com/H1bI9fo.png";
-    const cartoonStarCluster = "https://i.imgur.com/0xoT3Ky.png";
-    const rainbowStar = "https://i.imgur.com/qeOmPc0.png";
-
-    return(
-      <div
-      className="scroll-test"
-      style={{
-        height: "100vh", // try 100% here
-        width: "100vw",
-        overflowY: "scroll",
-        zIndex: 1,
-        top: 0,
-        left: 0,
-      }}
-    >
+  return (
+    <div className="galaxy">
       <img src="https://i.imgur.com/u9QduVC.png" />
     </div>
-    );
+  );
+}
 
-  };
-
-  export default Galaxy;
-
+export default Galaxy;
